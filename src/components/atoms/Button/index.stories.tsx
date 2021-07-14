3d46ae4488e7a1532bb0c5f3dc2ebe3095e67a0d
r-matsumoto <r-matsumoto@kdl.co.jp>
2021-07-14 10:30:22 +0900

    figmaとstorybookの連携

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 import { MyButton, Props } from "../Button";
+import { withDesign } from "storybook-addon-designs";
 
 export default {
   title: "Design System/Atoms/Button",
   component: MyButton,
-  decorators: [(story: any) => <div style={{ padding: "0 2rem" }}>{story()}</div>],
+  decorators: [(story: any) => <div style={{ padding: "0 2rem" }}>{story()}</div>, withDesign],
   argTypes: {
     schema: {
       options: ["blue", "red", "green", "purple"],
@@ -18,6 +19,12 @@ const Template: Story<Props> = (args) => <MyButton {...args}>sample</MyButton>;
 
 export const Blue = Template.bind({});
 Blue.args = {};
+Blue.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/uGEYjP7DYjxgOKjGiYbP5P/portfolio?node-id=2%3A16"
+  }
+}
 
 export const Red = Template.bind({});
 Red.args = {
