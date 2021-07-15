4ceab7dfa0ec5f80d262d33fba347316b3f5694f
r-matsumoto <r-matsumoto@kdl.co.jp>
2021-07-16 00:43:13 +0900

    fix

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,4 +1,4 @@
 // button
 export type ButtonVariant = 'solid' | 'outline'
 export type ButtonScheme = 'primary' | 'secondary' | 'danger' | 'info'
-export type Size = "sm" | "md" | "lg" | "xl" | "2xl" | "full" | "2xs" | "xs"
+export type Size = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full' | '2xs' | 'xs'
