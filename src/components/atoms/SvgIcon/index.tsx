a29cd91d5cd68053575b95ec382301396359d318
Matsumoto Ryota <awpjdmga1943@icloud.com>
2021-07-18 15:16:25 +0900

    RoadmapListItem実装の実装

--- src/components/atoms/SvgIcon/index.tsx
+++ src/components/atoms/SvgIcon/index.tsx
@@ -32,8 +32,8 @@ export const SvgIcon = memo(({ name, width, height }: Props) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M7.99984 15.3334C3.94975 15.3334 0.666504 12.0502 0.666504 8.00008C0.666504 3.94999 3.94975 0.666748 7.99984 0.666748C12.0499 0.666748 15.3332 3.94999 15.3332 8.00008C15.3332 12.0502 12.0499 15.3334 7.99984 15.3334ZM7.99984 14.0001C11.3135 14.0001 13.9998 11.3138 13.9998 8.00008C13.9998 4.68637 11.3135 2.00008 7.99984 2.00008C4.68613 2.00008 1.99984 4.68637 1.99984 8.00008C1.99984 11.3138 4.68613 14.0001 7.99984 14.0001ZM6.6665 9.05727L10.1951 5.52868L11.1379 6.47149L6.6665 10.9429L4.1951 8.47149L5.13791 7.52868L6.6665 9.05727Z"
             fill="url(#paint0_linear)"
           />
@@ -46,8 +46,8 @@ export const SvgIcon = memo(({ name, width, height }: Props) => {
               y2="15.3334"
               gradientUnits="userSpaceOnUse"
             >
-              <stop stop-color="#4ABEFF" />
-              <stop offset="1" stop-color="#009AF0" />
+              <stop stopColor="#4ABEFF" />
+              <stop offset="1" stopColor="#009AF0" />
             </linearGradient>
           </defs>
         </svg>
