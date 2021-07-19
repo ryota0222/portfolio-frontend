521f4abc61189b3390d930d0c5a834963786f0f5
Matsumoto Ryota <awpjdmga1943@icloud.com>
2021-07-20 00:24:54 +0900

    RoadmapMenuの実装

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -1,5 +1,12 @@
-import { CounterType, SvgIconName } from '@/types/interface'
+import { CounterType, RoadmapType, SvgIconName } from '@/types/interface'
 
-export const SVG_ICON_NAME_LIST: SvgIconName[] = ['circle-check']
+export const SVG_ICON_NAME_LIST: SvgIconName[] = [
+  'circle-check',
+  'schedule',
+  'develop',
+  'merge',
+]
 
 export const COUNTER_TYPE: CounterType[] = ['good', 'bad']
+
+export const ROADMAP_TYPE: RoadmapType[] = ['schedule', 'develop', 'merge']
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -4,7 +4,10 @@ export type ButtonScheme = 'primary' | 'secondary' | 'danger' | 'info'
 export type Size = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full' | '2xs' | 'xs'
 
 // svg icon
-export type SvgIconName = 'circle-check'
+export type SvgIconName = 'circle-check' | 'schedule' | 'develop' | 'merge'
 
 // counter
 export type CounterType = 'good' | 'bad'
+
+// roadmap
+export type RoadmapType = 'schedule' | 'develop' | 'merge'
