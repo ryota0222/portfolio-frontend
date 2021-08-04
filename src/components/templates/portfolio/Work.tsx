324ee0a664d88d2247748ec914acd04048c0359b
r-matsumoto <r-matsumoto@kdl.co.jp>
2021-08-04 23:55:35 +0900

    ダミーデータ流しこみ

--- src/components/templates/portfolio/Work.tsx
+++ src/components/templates/portfolio/Work.tsx
@@ -1,5 +1,8 @@
-import { Box } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { Box, Text, Flex, Fade, Portal } from '@chakra-ui/react'
+import { CgMouse } from 'react-icons/cg'
 import { PortfolioWorks } from '@/apis/models'
+import { PageWrapper } from '@/styles/globals'
 
 interface Props {
   data?: {
@@ -9,7 +12,58 @@ interface Props {
 }
 
 const PortfolioWorkTemplate: React.FC<Props> = ({ data }) => {
-  return <Box>Portfolio</Box>
+  const [isReachBottom, setIsReachBottom] = useState(true)
+  useEffect(() => {
+    document.addEventListener('scroll', trackScroll)
+    return () => {
+      document.removeEventListener('scroll', trackScroll)
+    }
+  }, [])
+  // スクロール時の処理
+  const trackScroll = () => {
+    const ele = document.getElementById('portfolio-container')
+    console.log(ele.scrollHeight)
+    console.log(ele.scrollTop)
+    console.log(ele.clientHeight)
+    // スクロールがそこに達したら実行
+    if (ele.scrollHeight - ele.scrollTop === ele.clientHeight) {
+      console.log('header bottom reached')
+      setIsReachBottom(true)
+    } else if (isReachBottom) {
+      setIsReachBottom(false)
+    }
+  }
+  // データ
+  const _data = data?.data
+  if (!_data || _data?.length <= 0) {
+    // TODO: データがない場合
+    return <></>
+  }
+  return (
+    <PageWrapper id="portfolio-container">
+      {/* コンテンツ */}
+      {_data.map((item, idx) => {
+        return (
+          <Box key={`portfolio-work-${idx}`}>
+            <PageWrapper>{item.title}</PageWrapper>
+          </Box>
+        )
+      })}
+      {/* スクロールアイコン */}
+      <Portal>
+        <Box position="fixed" left="0" bottom="8px" w="full">
+          <Fade in={isReachBottom}>
+            <Flex flexDirection="column" alignItems="center">
+              <CgMouse size={20} color="#C4C4C4" />
+              <Text color="#C4C4C4" fontWeight="bold" fontSize="12">
+                scroll Down
+              </Text>
+            </Flex>
+          </Fade>
+        </Box>
+      </Portal>
+    </PageWrapper>
+  )
 }
 
 export default PortfolioWorkTemplate
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import * as apis from '@/apis/api'
 import PortfolioWorkTemplate from '@/components/templates/portfolio/Work'
+import { work as DAMMY_WORK } from '@/consts/dammy/portfolio'
 import { HeadComponent } from '@/utils/head'
 
 const Portfolio = ({ data }) => {
@@ -13,12 +14,16 @@ const Portfolio = ({ data }) => {
 }
 
 export const getStaticProps = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    return { props: { data: DAMMY_WORK } }
+  }
   const offset = 0
   const limit = 100
   const func = await apis.PortfolioApiFp().getPortfolioWorks(offset, limit)
   const data = await func()
   console.log('data')
   console.log(data.data)
+  console.log(typeof data.data)
   return { props: { data } }
 }
 
