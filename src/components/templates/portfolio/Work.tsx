a001d00c28851463ffe83cfc84362b570f73b3c1
r-matsumoto <r-matsumoto@kdl.co.jp>
2021-08-08 17:41:39 +0900

    API接続

--- src/components/templates/portfolio/Work.tsx
+++ src/components/templates/portfolio/Work.tsx
@@ -1,6 +1,22 @@
 import { useEffect, useState } from 'react'
-import { Box, Text, Flex, Fade, Portal } from '@chakra-ui/react'
+import {
+  Box,
+  Text,
+  Flex,
+  Fade,
+  Portal,
+  useBreakpointValue,
+  Center,
+  useColorModeValue,
+  TypographyProps,
+} from '@chakra-ui/react'
 import { CgMouse } from 'react-icons/cg'
+import LazyLoad from 'react-lazyload'
+import {
+  CSSTransition,
+  TransitionGroup,
+  Transition,
+} from 'react-transition-group'
 import { PortfolioWorks } from '@/apis/models'
 import { PageWrapper } from '@/styles/globals'
 
@@ -12,9 +28,35 @@ interface Props {
 }
 
 const PortfolioWorkTemplate: React.FC<Props> = ({ data }) => {
-  const [isReachBottom, setIsReachBottom] = useState(true)
+  const [isReachBottom, setIsReachBottom] = useState(false)
+  const [innerHeight, setInnerHeight] = useState(900)
+  const width = useBreakpointValue({ base: '100%', sm: '50%' })
+  const transform = useBreakpointValue({ base: '0deg', sm: '-90deg' })
+  const yearFlex = useBreakpointValue({ base: 'initial', sm: 'flex' })
+  const yearLeft = useBreakpointValue({ base: 0, sm: '24px' })
+  const yearPx = useBreakpointValue({ base: 4, sm: 0 })
+  const yearMx = useBreakpointValue({ base: 4, sm: 0 })
+  const yearW = useBreakpointValue({ base: 'full', sm: 'initial' })
+  const yearBorderW = useBreakpointValue({ base: 'auto', sm: '300px' })
+  const yearMl = useBreakpointValue({ base: 0, sm: 2 })
+  const textAlign: TypographyProps['textAlign'] = useBreakpointValue({
+    base: 'right',
+    sm: 'initial',
+  })
+  const descriptionFontSize = useBreakpointValue({ base: 'sm', sm: 'md' })
+  const titleMb = useBreakpointValue({ base: 2, sm: 6 })
+  const color = useColorModeValue('#002E48', '#FFFFFF')
+  const flexDirection: 'column' | 'row' = useBreakpointValue({
+    base: 'column',
+    sm: 'row',
+  })
+  const minHeight = useBreakpointValue({
+    base: 'auto',
+    sm: 'calc(100vh - 72px)',
+  })
   useEffect(() => {
     document.addEventListener('scroll', trackScroll)
+    setInnerHeight(window?.innerHeight)
     return () => {
       document.removeEventListener('scroll', trackScroll)
     }
@@ -22,14 +64,10 @@ const PortfolioWorkTemplate: React.FC<Props> = ({ data }) => {
   // スクロール時の処理
   const trackScroll = () => {
     const ele = document.getElementById('portfolio-container')
-    console.log(ele.scrollHeight)
-    console.log(ele.scrollTop)
-    console.log(ele.clientHeight)
     // スクロールがそこに達したら実行
-    if (ele.scrollHeight - ele.scrollTop === ele.clientHeight) {
-      console.log('header bottom reached')
+    if (ele.getBoundingClientRect().bottom <= window.innerHeight) {
       setIsReachBottom(true)
-    } else if (isReachBottom) {
+    } else if (!isReachBottom) {
       setIsReachBottom(false)
     }
   }
@@ -44,15 +82,77 @@ const PortfolioWorkTemplate: React.FC<Props> = ({ data }) => {
       {/* コンテンツ */}
       {_data.map((item, idx) => {
         return (
-          <Box key={`portfolio-work-${idx}`}>
-            <PageWrapper>{item.title}</PageWrapper>
-          </Box>
+          <LazyLoad key={`portfolio-work-${idx}`} height={innerHeight} once>
+            <TransitionGroup>
+              <CSSTransition timeout={500} classNames="fade" appear={true}>
+                <Box>
+                  <PageWrapper>
+                    <Flex
+                      w="full"
+                      minHeight={minHeight}
+                      flexDirection={flexDirection}
+                    >
+                      {/* 画像 */}
+                      <Box w={width} minHeight={minHeight} position="relative">
+                        {/* 年月 */}
+                        <Box
+                          display={yearFlex}
+                          transform={`rotate(${transform})`}
+                          position="absolute"
+                          left={yearLeft}
+                          bottom="0"
+                          alignItems="center"
+                          transformOrigin="left"
+                          w={yearW}
+                        >
+                          <Text
+                            fontFamily="'Josefin Sans'"
+                            fontSize="1.5rem"
+                            textAlign={textAlign}
+                            mx={yearPx}
+                            boxSizing="border-box"
+                          >
+                            {item.year}
+                          </Text>
+                          <CSSTransition
+                            classNames="line"
+                            timeout={500}
+                            appear={true}
+                          >
+                            <Box
+                              w={yearBorderW}
+                              height="1px"
+                              bgColor={color}
+                              ml={yearMl}
+                              mx={yearMx}
+                            ></Box>
+                          </CSSTransition>
+                        </Box>
+                      </Box>
+                      <Center w={width} minHeight={minHeight}>
+                        <Box boxSizing="border-box" px="4" maxW="500px">
+                          {/* タイトル */}
+                          <Text fontSize="4xl" fontWeight="bold" mb={titleMb}>
+                            {item.title}
+                          </Text>
+                          {/* 概要文 */}
+                          <Text fontSize={descriptionFontSize}>
+                            {item.description}
+                          </Text>
+                        </Box>
+                      </Center>
+                    </Flex>
+                  </PageWrapper>
+                </Box>
+              </CSSTransition>
+            </TransitionGroup>
+          </LazyLoad>
         )
       })}
       {/* スクロールアイコン */}
       <Portal>
         <Box position="fixed" left="0" bottom="8px" w="full">
-          <Fade in={isReachBottom}>
+          <Fade in={!isReachBottom}>
             <Flex flexDirection="column" alignItems="center">
               <CgMouse size={20} color="#C4C4C4" />
               <Text color="#C4C4C4" fontWeight="bold" fontSize="12">
