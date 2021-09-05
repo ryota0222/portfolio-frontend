46bdef40c8f81c9700b4fdd67e27b51624290483
r-matsumoto <r-matsumoto@kdl.co.jp>
2021-09-05 22:39:42 +0900

    next.config.jsのパッケージのパッケージをまとめる

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+
+const withPlugins = require('next-compose-plugins');
+
 const withVideos = require('next-videos')
 
-module.exports = withVideos({
-  reactStrictMode: true,
+module.exports = withPlugins([withBundleAnalyzer, withVideos], {
+  /* オプション設定 */
   images: {
     domains: ['images.ctfassets.net'],
   },
 })
-
-module.exports = withBundleAnalyzer({})
