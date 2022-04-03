b0e826e7361100298218829a79422671fffe9dbc
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-04-03 19:03:19 +0900

    refactor: Split const files by role

---
+++ src/consts/meta.ts
@@ -0,0 +1,6 @@
+export const TITLE = 'RyoTa.'
+export const DESCRIPTION =
+  'RyoTa.のポートフォリオサイトです。2020年よりフロントエンジニアとしてUI/UXの発信をしています。Adobe XDやFigmaを用いたデザインモックの作成や、ツールを用いた素材やアニメーションの作成もしています。'
+export const KEYWORD = 'エンジニアポートフォリオ,ブログ,IT'
+export const IMAGE = `${process.env.NEXT_PUBLIC_SITE_URL}/images/mv.png`
+export const URL = 'RyoTa.'
---
+++ src/consts/style.ts
@@ -0,0 +1,26 @@
+import { SvgIconName } from '@/types/interface'
+
+export const ROOT_FONT_SIZE = 16
+
+export const SCROLL_HEIGHT = 'calc(var(--vh, 1vh) * 100 - 48px)'
+
+export const FULL_HEIGHT = 'calc(var(--vh, 1vh) * 100)'
+
+export const GRADIENT =
+  '-webkit-linear-gradient(38.45deg, #9E00FF -1.95%, rgba(255, 102, 212, 0.984375) 16.64%, rgba(233, 172, 255, 0) 49.6%), -webkit-linear-gradient(249.25deg, rgba(0, 255, 224, 0.79) -23.07%, rgba(162, 255, 89, 0.79) 13.65%, rgba(255, 214, 0, 0.11) 46.38%), -webkit-linear-gradient(158.28deg, #FF0000 -8.44%, #FF9A2B 44.44%, #FAFF00 96.23%);'
+
+export const SVG_ICON_NAME_LIST: SvgIconName[] = [
+  'circle-check',
+  'schedule',
+  'develop',
+  'merge',
+  'shirt',
+  'balloon',
+  'phone',
+  'link',
+  'create',
+  'update',
+  'home',
+  'twitter',
+  'facebook',
+]
