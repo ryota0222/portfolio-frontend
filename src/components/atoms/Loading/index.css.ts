69ab2e5c6c9904df74397f269e29673422e8e47c
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-04-17 00:23:57 +0900

    loadingコンポーネントのcss修正

--- src/components/atoms/Loading/index.css.ts
+++ src/components/atoms/Loading/index.css.ts
@@ -2,10 +2,8 @@ import styled from 'styled-components'
 
 export const Animation = styled.div<{ width: string; height: string }>`
   display: inline-block;
-  img {
-    width: ${(props) => props.width};
-    height: ${(props) => props.height};
-  }
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   animation: hueRotate 5s infinite, rotate 2.5s infinite;
   @keyframes hueRotate {
     0% {
