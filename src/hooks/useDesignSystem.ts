44710a968f344ed9f9089d7c53af3f366c0cfbe8
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-05-27 02:02:16 +0900

    feat: Add isDark flg to useDesignSystem hook

--- src/hooks/useDesignSystem.ts
+++ src/hooks/useDesignSystem.ts
@@ -1,14 +1,17 @@
 import { useColorModeValue } from '@chakra-ui/react'
 
 type UseDesignSystem = () => {
+  isDark: boolean
   NO_DATA_COLOR: string
   BLOG_SIDE_MENU_BG: string
 }
 
 const useDesignSystem: UseDesignSystem = () => {
+  const isDark = useColorModeValue(false, true)
   const NO_DATA_COLOR = useColorModeValue('#999', '#ccc')
   const BLOG_SIDE_MENU_BG = useColorModeValue('#F0F0F0', '#252829')
   return {
+    isDark,
     NO_DATA_COLOR,
     BLOG_SIDE_MENU_BG,
   }
