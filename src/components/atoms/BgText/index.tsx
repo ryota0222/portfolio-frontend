db01a85c375645cb271f353e5d6fa32943e63b08
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-05-31 00:29:03 +0900

    Add component of BgText

---
+++ src/components/atoms/BgText/index.tsx
@@ -0,0 +1,40 @@
+import React, { memo, useMemo } from 'react'
+import { Box } from '@chakra-ui/react'
+import useDesignSystem from '@/hooks/useDesignSystem'
+import { Size } from './type'
+
+interface Props {
+  size: Size
+}
+
+export const BgText: React.FC<Props> = memo(({ size }) => {
+  const { isDark, BG_COLOR: textColor } = useDesignSystem()
+  const textShadow = useMemo(() => {
+    if (size === 'lg') {
+      return isDark
+        ? '6px 4px 21px #222222, -6px -6px 16px #666666'
+        : '6px 4px 12px #E6E6E6, -6px -6px 12px #FFFFFF'
+    } else {
+      return isDark
+        ? '3px 2px 11px #222222, -3px -4px 8px #666666'
+        : '3px 2px 5px #E6E6E6, -4px -4px 6px #FFFFFF'
+    }
+  }, [isDark, size])
+  const fontSize = useMemo(() => {
+    if (size === 'lg') return '9xl'
+    else return '6xl'
+  }, [size])
+  return (
+    <Box
+      color={textColor}
+      textShadow={textShadow}
+      fontSize={fontSize}
+      fontFamily="'Josefin Sans'"
+      fontWeight={'bold'}
+      d="inline-block"
+      transform="rotate(-90deg)"
+    >
+      RyoTa.
+    </Box>
+  )
+})
--- src/hooks/useDesignSystem.ts
+++ src/hooks/useDesignSystem.ts
@@ -5,6 +5,7 @@ type UseDesignSystem = () => {
   NO_DATA_COLOR: string
   BLOG_SIDE_MENU_BG: string
   TEXT_COLOR: string
+  BG_COLOR: string
 }
 
 const useDesignSystem: UseDesignSystem = () => {
@@ -12,11 +13,13 @@ const useDesignSystem: UseDesignSystem = () => {
   const NO_DATA_COLOR = useColorModeValue('app-gray.200', 'app-gray.800')
   const BLOG_SIDE_MENU_BG = useColorModeValue('app-gray.600', 'app-gray.500')
   const TEXT_COLOR = useColorModeValue('app-gray.900', 'app-gray.50')
+  const BG_COLOR = useColorModeValue('app-gray.50', 'app-gray.700')
   return {
     isDark,
     NO_DATA_COLOR,
     BLOG_SIDE_MENU_BG,
     TEXT_COLOR,
+    BG_COLOR,
   }
 }
 
