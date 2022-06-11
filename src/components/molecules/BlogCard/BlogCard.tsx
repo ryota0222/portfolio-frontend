eb8d5d911e700aa2f98d41b133f19ace16b0c8b7
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-06-11 12:01:48 +0900

    feat: Fix BlogCard styling of sp

--- src/components/molecules/BlogCard/BlogCard.tsx
+++ src/components/molecules/BlogCard/BlogCard.tsx
@@ -1,79 +1,78 @@
-import React, { memo, useMemo } from 'react'
-import { Text, Box, Skeleton, SkeletonText } from '@chakra-ui/react'
+import React, { memo } from 'react'
+import { Text, Box, Skeleton, Flex } from '@chakra-ui/react'
 import Image from 'next/image'
 import { BlogCategory } from '@/components/atoms/BlogCategory'
 import useDesignSystem from '@/hooks/useDesignSystem'
 import { Props } from './type'
+import { useBlogCard } from './useBlogCard'
 
 export const BlogCard: React.FC<Props> = memo(
   ({ title, icon, label, loading, image }) => {
-    const { TEXT_COLOR, isDark } = useDesignSystem()
-    const startColor = useMemo(() => {
-      return isDark ? 'app-gray.600' : 'app-gray.300'
-    }, [isDark])
-    const endColor = useMemo(() => {
-      return isDark ? 'app-gray.500' : 'app-gray.500'
-    }, [isDark])
+    const { TEXT_COLOR } = useDesignSystem()
+    const styles = useBlogCard()
     // 取得中
     if (loading) {
       return (
-        <Box>
+        <Flex flexDirection={styles.wrapperFlexDir}>
           <Skeleton
-            width="200px"
-            height="150px"
+            width={styles.imageSize.width}
+            height={styles.imageSize.height}
             borderRadius="md"
-            startColor={startColor}
-            endColor={endColor}
+            startColor={styles.skeltonColor.start}
+            endColor={styles.skeltonColor.end}
           />
-          <Skeleton
-            width="200px"
-            height="16px"
-            mt={2}
-            startColor={startColor}
-            endColor={endColor}
-          />
-          <Skeleton
-            width="60px"
-            height="16px"
-            mt={2}
-            startColor={startColor}
-            endColor={endColor}
-          />
-        </Box>
+          <Box sx={styles.textWrapperStyle}>
+            <Skeleton
+              width={styles.textMaxW}
+              height="16px"
+              mt={2}
+              startColor={styles.skeltonColor.start}
+              endColor={styles.skeltonColor.end}
+            />
+            <Skeleton
+              width="60px"
+              height="16px"
+              mt={2}
+              startColor={styles.skeltonColor.start}
+              endColor={styles.skeltonColor.end}
+            />
+          </Box>
+        </Flex>
       )
     }
     return (
-      <Box>
+      <Flex flexDirection={styles.wrapperFlexDir}>
         {/* 画像 */}
         <Box
-          width={'200px'}
-          height={'150px'}
+          width={styles.imageSize.width}
+          height={styles.imageSize.height}
           borderRadius="md"
           overflow={'hidden'}
         >
           <Image
             src={image}
             alt={title}
-            width={'200px'}
-            height={'150px'}
+            width={styles.imageSize.width}
+            height={styles.imageSize.height}
             objectFit={'cover'}
             loading={'lazy'}
             quality={100}
           />
         </Box>
-        <Text
-          color={TEXT_COLOR}
-          fontWeight="bold"
-          noOfLines={2}
-          mt={2}
-          mb={1}
-          maxW="200px"
-          fontSize="sm"
-        >
-          {title}
-        </Text>
-        <BlogCategory icon={icon} label={label} size="sm" />
-      </Box>
+        <Box sx={styles.textWrapperStyle}>
+          <Text
+            color={TEXT_COLOR}
+            fontWeight="bold"
+            noOfLines={2}
+            mb={1}
+            maxW={styles.textMaxW}
+            fontSize="sm"
+          >
+            {title}
+          </Text>
+          <BlogCategory icon={icon} label={label} size="sm" />
+        </Box>
+      </Flex>
     )
   },
 )
