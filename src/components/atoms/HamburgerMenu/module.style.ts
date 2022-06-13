2e62ab2c9731e075751b1f1cc517e5aeaa43621c
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-06-14 01:49:33 +0900

    feat: Create storybook of HamburgerMenu Component

---
+++ src/components/atoms/HamburgerMenu/module.style.ts
@@ -0,0 +1,46 @@
+import { Box } from '@chakra-ui/react'
+import styled from 'styled-components'
+
+export const HamburgerIcon = styled(Box)<{ width: string; height: string }>`
+  display: inline-block;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  position: relative;
+  span {
+    display: inline-block;
+    transition: all 0.4s;
+    position: absolute;
+    left: 0;
+    height: 3px;
+    border-radius: 2px;
+    width: 100%;
+    &:nth-of-type(1) {
+      top: 2px;
+    }
+    &:nth-of-type(2) {
+      top: 11px;
+    }
+    &:nth-of-type(3) {
+      top: 19px;
+    }
+  }
+  &.active {
+    span {
+      &:nth-of-type(1) {
+        top: 0px;
+        left: 0px;
+        transform: translateY(9.5px) rotate(-45deg);
+        width: 100%;
+      }
+      &:nth-of-type(2) {
+        opacity: 0;
+      }
+      &:nth-of-type(3) {
+        bottom: 0px;
+        right: 0px;
+        transform: translateY(-9.5px) rotate(45deg);
+        width: 100%;
+      }
+    }
+  }
+`
---
+++ src/components/atoms/HamburgerMenu/type.ts
@@ -0,0 +1,5 @@
+export interface Props {
+  isOpen: boolean
+}
+
+export interface UseHamburgerMenu {}
