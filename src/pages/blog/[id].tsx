6585c3d75297b092b50f5b277bb245a70f635f96
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-06-28 13:14:28 +0900

    fix: Fix ogp type

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -43,7 +43,7 @@ const BlogDetail = ({ data }) => {
         title={data.data.title}
         description={data.data.description}
         url={`${process.env.NEXT_PUBLIC_SITE_URL}/blog/${id}`}
-        ogType="article"
+        // ogType="article"
         image={`https:${data.data.image}?w=1200&h=630`}
       />
       <BlogDetailTemplate data={data.data} />
